02b37c7aa28081366837572a8a7c04dbd2971d4d
Weyoss <weyoss@protonmail.com>
2018-03-06 12:27:48 +0300

    Make redis params optional, support all redis.createClient params

--- src/redis-client.js
+++ src/redis-client.js
@@ -3,18 +3,13 @@
 const redis = require('redis');
 
 module.exports = {
-
     /**
      *
      * @param {object} config
      * @return {object}
      */
-    getNewInstance(config) {
-        if (!config.hasOwnProperty('redis') ||
-            !config.redis.hasOwnProperty('port') ||
-            !config.redis.hasOwnProperty('host')) {
-            throw new Error('Missing redis parameters.');
-        }
-        return redis.createClient(config.redis.port, config.redis.host);
+    getNewInstance(config = {}) {
+        const { redis: options = {} } = config;
+        return redis.createClient(options);
     },
 };
