1c5bf1042108b2e7aaf1d1b75b71530a83725cfc
Weyoss <weyoss@protonmail.com>
2018-03-06 19:12:16 +0300

    Refactor redis-keys module

--- src/redis-keys.js
+++ src/redis-keys.js
@@ -2,7 +2,30 @@
 
 let namespace = 'default';
 
+const keyTypes = {
+    KEY_TYPE_MESSAGE_QUEUE: '1.1',
+    KEY_TYPE_PROCESSING_QUEUE: '1.2',
+    KEY_TYPE_DEAD_LETTER_QUEUE: '1.3',
+    KEY_TYPE_HEARTBEAT: '2.1',
+    KEY_TYPE_GC_LOCK: '3.1',
+    KEY_TYPE_GC_LOCK_TMP: '3.2',
+    KEY_TYPE_RATE_INPUT: '4.1',
+    KEY_TYPE_RATE_PROCESSING: '4.2',
+    KEY_TYPE_RATE_ACKNOWLEDGED: '4.3',
+    KEY_TYPE_RATE_UNACKNOWLEDGED: '4.4',
+    KEY_TYPE_STATS_FRONTEND_LOCK: '5.1',
+};
+
+
 module.exports = {
+
+    /**
+     * 
+     * @return {object}
+     */
+    getKeyTypes() {
+        return keyTypes;
+    },
 
     /**
      *
@@ -21,7 +44,7 @@ module.exports = {
         if (typeof part !== 'string' || !part.length) {
             throw new Error('Redis key validation error. Expected be a non empty string.');
         }
-        const filtered = part.toLowerCase().replace(/[^a-z0-9_-]/gi, '');
+        const filtered = part.toLowerCase().replace(/[^a-z0-9_-]/g, '');
         if (filtered.length !== part.length) {
             throw new Error('Redis key validation error. Expected only letters (a-z), numbers (0-9) and (-_)');
         }
@@ -39,40 +62,44 @@ module.exports = {
     getKeys(args = {}) {
         let { queueName } = args;
         const { consumerId, producerId } = args;
-        if (queueName && queueName.indexOf('queue:') === 10) {
-            queueName = queueName.split(':')[2];
+        if (queueName && queueName.indexOf(`|@${keyTypes.KEY_TYPE_MESSAGE_QUEUE}|`) > 0) {
+            queueName = queueName.split('|')[2].replace(/[@]/g, '');
         }
         const keys = {};
-        keys.patternQueueNameDead = 'dead:*';
-        keys.patternQueueNameProcessing = 'processing:*';
-        keys.patternQueueName = 'queue:*';
-        keys.patternHeartBeat = 'heartbeat:*';
-        keys.patternGC = 'gc:*';
-        keys.patternRate = 'rate:*';
-        keys.patternRateProcessing = 'rate:processing:*';
-        keys.patternRateAcknowledged = 'rate:acknowledged:*';
-        keys.patternRateUnacknowledged = 'rate:unacknowledged:*';
-        keys.patternRateInput = 'rate:input:*';
-        keys.keyStatsFrontendLock = 'stats:frontend:lock';
+
+        /**
+         * Key patterns
+         */
+        keys.patternQueueNameDead = `@${keyTypes.KEY_TYPE_DEAD_LETTER_QUEUE}|*`;
+        keys.patternQueueNameProcessing = `@${keyTypes.KEY_TYPE_PROCESSING_QUEUE}|*`;
+        keys.patternQueueName = `@${keyTypes.KEY_TYPE_MESSAGE_QUEUE}|*`;
+        keys.patternHeartBeat = `@${keyTypes.KEY_TYPE_HEARTBEAT}|*`;
+        keys.patternGC = '@3.*';
+        keys.patternRate = '@4.*';
+        keys.patternRateProcessing = `@${keyTypes.KEY_TYPE_RATE_PROCESSING}|*`;
+        keys.patternRateAcknowledged = `@${keyTypes.KEY_TYPE_RATE_ACKNOWLEDGED}|*`;
+        keys.patternRateUnacknowledged = `@${keyTypes.KEY_TYPE_RATE_UNACKNOWLEDGED}|*`;
+        keys.patternRateInput = `@${keyTypes.KEY_TYPE_RATE_INPUT}|*`;
+        keys.keyStatsFrontendLock = `@${keyTypes.KEY_TYPE_STATS_FRONTEND_LOCK}`;
         if (queueName) {
-            keys.patternQueueNameProcessing = `processing:${queueName}:*`;
-            keys.keyQueueName = `queue:${queueName}`;
-            keys.keyQueueNameDead = `dead:${queueName}`;
-            keys.keyGCLock = `gc:${queueName}:lock`;
-            keys.keyGCLockTmp = `${keys.keyGCLock}:tmp`;
+            keys.patternQueueNameProcessing = `@${keyTypes.KEY_TYPE_PROCESSING_QUEUE}|${queueName}|*`;
+            keys.keyQueueName = `@${keyTypes.KEY_TYPE_MESSAGE_QUEUE}|${queueName}`;
+            keys.keyQueueNameDead = `@${keyTypes.KEY_TYPE_DEAD_LETTER_QUEUE}|${queueName}`;
+            keys.keyGCLock = `@${keyTypes.KEY_TYPE_GC_LOCK}|${queueName}`;
+            keys.keyGCLockTmp = `@${keyTypes.KEY_TYPE_GC_LOCK_TMP}|${queueName}`;
             if (consumerId) {
-                keys.keyQueueNameProcessing = `processing:${queueName}:${consumerId}`;
-                keys.keyHeartBeat = `heartbeat:${queueName}:${consumerId}`;
-                keys.keyRateProcessing = `rate:processing:${queueName}:${consumerId}`;
-                keys.keyRateAcknowledged = `rate:acknowledged:${queueName}:${consumerId}`;
-                keys.keyRateUnacknowledged = `rate:unacknowledged:${queueName}:${consumerId}`;
+                keys.keyQueueNameProcessing = `@${keyTypes.KEY_TYPE_PROCESSING_QUEUE}|${queueName}|${consumerId}`;
+                keys.keyHeartBeat = `@${keyTypes.KEY_TYPE_HEARTBEAT}|${queueName}|${consumerId}`;
+                keys.keyRateProcessing = `@${keyTypes.KEY_TYPE_RATE_PROCESSING}|${queueName}|${consumerId}`;
+                keys.keyRateAcknowledged = `@${keyTypes.KEY_TYPE_RATE_ACKNOWLEDGED}|${queueName}|${consumerId}`;
+                keys.keyRateUnacknowledged = `@${keyTypes.KEY_TYPE_RATE_UNACKNOWLEDGED}|${queueName}|${consumerId}`;
             }
             if (producerId) {
-                keys.keyRateInput = `rate:input:${queueName}:${producerId}`;
+                keys.keyRateInput = `@${keyTypes.KEY_TYPE_RATE_INPUT}|${queueName}|${producerId}`;
             }
         }
         const ns = `redis-smp-${namespace}`;
-        for (const k in keys) keys[k] = `${ns}:${keys[k]}`;
+        for (const k in keys) keys[k] = `${ns}|${keys[k]}`;
         return keys;
     },
 
@@ -82,31 +109,42 @@ module.exports = {
      * @returns {object}
      */
     getKeySegments(key) {
-        if (key.indexOf('processing:') === 10) {
-            const [, , queueName, consumerId] = key.split(':');
+        const segments = key.split('|');
+        if (segments[1].indexOf(`@${keyTypes.KEY_TYPE_PROCESSING_QUEUE}`) === 0) {
+            const [, , queueName, consumerId] = segments;
             return {
+                type: keyTypes.KEY_TYPE_PROCESSING_QUEUE,
                 queueName,
                 consumerId,
             };
         }
-        if (key.indexOf('rate:') === 10) {
-            const [, , type, queueName, id] = key.split(':');
+        if (segments[1].indexOf('@4.') === 0) {
+            const [, , type, queueName, id] = segments;
             return {
-                type,
+                type: type.replace(/[@]/g, ''),
                 queueName,
                 id,
             };
         }
-        if (key.indexOf('heartbeat:') === 10) {
-            const [, , queueName, consumerId] = key.split(':');
+        if (segments[1].indexOf(`@${keyTypes.KEY_TYPE_HEARTBEAT}`) === 0) {
+            const [, , queueName, consumerId] = segments;
             return {
+                type: keyTypes.KEY_TYPE_HEARTBEAT,
                 queueName,
                 consumerId,
             };
         }
-        if (key.indexOf('dead:') === 10 || key.indexOf('queue:') === 10) {
-            const [, , queueName] = key.split(':');
+        if (segments[1].indexOf(`@${keyTypes.KEY_TYPE_MESSAGE_QUEUE}`) === 0) {
+            const [, , queueName] = segments;
             return {
+                type: keyTypes.KEY_TYPE_MESSAGE_QUEUE,
+                queueName,
+            };
+        }
+        if (segments[1].indexOf(`@${keyTypes.KEY_TYPE_DEAD_LETTER_QUEUE}`) === 0) {
+            const [, , queueName] = segments;
+            return {
+                type: keyTypes.KEY_TYPE_DEAD_LETTER_QUEUE,
                 queueName,
             };
         }
