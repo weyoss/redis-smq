04861d837248d8077d604d6121739388acad40f8
Weyoss <weyoss@outlook.com>
2018-03-09 19:12:20 +0300

    Use hashes/hgetall instead of scanning with match pattern.

--- src/heartbeat.js
+++ src/heartbeat.js
@@ -49,7 +49,13 @@ function heartBeat(consumer) {
                     },
                     cpu: process.cpuUsage(),
                 };
-                client.setex(keyHeartBeat, 10, JSON.stringify(usage), (err) => {
+                const timestamp = Date.now();
+                const payload = JSON.stringify({
+                    timestamp,
+                    usage,
+                });
+                const hashKey = `${queueName}|${consumerId}`;
+                client.hset(keyHeartBeat, hashKey, payload, (err, result) => {
                     if (err) consumer.emit('error', err);
                     else {
                         setTimeout(() => {
@@ -70,10 +76,22 @@ function heartBeat(consumer) {
  * @param {function} cb
  */
 heartBeat.isOnline = function isOnline(client, queueName, consumerId, cb) {
-    const keys = redisKeys.getKeys({ queueName, consumerId });
-    client.get(keys.keyHeartBeat, (err, res) => {
+    const keys = redisKeys.getKeys();
+    const hashKey = `${queueName}|${consumerId}`;
+    client.hget(keys.keyHeartBeat, hashKey, (err, res) => {
         if (err) cb(err);
-        else cb(null, !!res);
+        else {
+            let online = false;
+            if (res) {
+                const now = Date.now();
+                const payload = JSON.parse(res);
+                const { timestamp } = payload;
+                online = (now - timestamp <= 10000);
+                // Do not wait for keys deletion, reply as fast as possible
+                if (!online) client.hdel(keys.keyHeartBeat, hashKey);
+            }
+            cb(null, online);
+        }
     });
 };
 
@@ -83,36 +101,29 @@ heartBeat.isOnline = function isOnline(client, queueName, consumerId, cb) {
  * @param {function} cb
  */
 heartBeat.getOnlineConsumers = function getOnlineConsumers(client, cb) {
-    let heartBeatKeys = [];
-    const consumers = {};
     const rKeys = redisKeys.getKeys();
-    const onConsumers = (err, values) => {
+    const consumers = {};
+    const deadConsumers = [];
+    client.hgetall(rKeys.keyHeartBeat, (err, result) => {
         if (err) cb(err);
-        else {
-            values.forEach((item, index) => {
-                if (item) {
-                    const segments = redisKeys.getKeySegments(heartBeatKeys[index]);
-                    if (!consumers.hasOwnProperty(segments.queueName)) (consumers[segments.queueName] = []);
-                    consumers[segments.queueName].push({
-                        consumerId: segments.consumerId,
-                        resources: JSON.parse(item),
+        else if (result) {
+            const now = Date.now();
+            for (const hashKey in result) {
+                const { timestamp, usage: resources } = JSON.parse(result[hashKey]);
+                if (now - timestamp <= 10000) {
+                    const [queueName, consumerId] = hashKey.split('|');
+                    if (!consumers.hasOwnProperty(queueName)) (consumers[queueName] = []);
+                    consumers[queueName].push({
+                        consumerId,
+                        resources,
                     });
-                }
-            });
+                } else deadConsumers.push(hashKey);
+            }
+            // Do not wait for keys deletion, reply as fast as possible
+            if (deadConsumers.length) client.hdel(rKeys.keyHeartBeat, ...deadConsumers);
             cb(null, consumers);
-        }
-    };
-    const onScanResults = (err, res0) => {
-        if (err) cb(err);
-        else {
-            const [cur, keys] = res0;
-            if (keys && keys.length) {
-                heartBeatKeys = keys;
-                client.mget(keys, onConsumers);
-            } else cb(null, consumers);
-        }
-    };
-    client.scan('0', 'match', rKeys.patternHeartBeat, 'count', 1000, onScanResults);
+        } else cb(null, consumers);
+    });
 };
 
 module.exports = heartBeat;
