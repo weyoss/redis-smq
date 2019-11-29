19ca2302534618f08f533a12e73edcb94bc60a97
Weyoss <weyoss@outlook.com>
2019-11-29 07:53:36 -0800

    Fix broken message retry delay

--- test/test15.test.js
+++ test/test15.test.js
@@ -10,14 +10,15 @@ const {
 
 
 test('A consumer delays a failed message before re-queuing it again, given messageRetryThreshold is not exceeded', async () => {
-    const consumer = getConsumer('test_queue', { messageRetryDelay: 10 });
-
+    const consumer = getConsumer('test_queue', { messageRetryDelay: 10, messageRetryThreshold: 5 });
+    const timestamps = [];
     let callCount = 0;
     const mock = jest.fn((msg, cb) => {
+        timestamps.push(Date.now());
         callCount += 1;
-        if (callCount === 1) {
+        if (callCount < 5) {
             throw new Error('Explicit error');
-        } else if (callCount === 2) {
+        } else if (callCount === 5) {
             cb();
         } else throw new Error('Unexpected call');
     });
@@ -40,7 +41,22 @@ test('A consumer delays a failed message before re-queuing it again, given messa
     await onMessageConsumed(consumer, () => {});
 
     await onConsumerIdle(consumer, () => {
-        expect(delayedCount).toBe(1);
+        expect(delayedCount).toBe(4);
         expect(consumedCount).toBe(1);
     });
+
+    for (let i = 0; i < timestamps.length; i += 1) {
+        const diff = timestamps[i] - timestamps[0];
+        if (i === 0) {
+            expect(validateTime(diff, 0)).toBe(true);
+        } else if (i === 1) {
+            expect(validateTime(diff, 10000)).toBe(true);
+        } else if (i === 2) {
+            expect(validateTime(diff, 20000)).toBe(true);
+        } else if (i === 3) {
+            expect(validateTime(diff, 30000)).toBe(true);
+        } else {
+            expect(validateTime(diff, 40000)).toBe(true);
+        }
+    }
 });
