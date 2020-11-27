2b5ff6b0973c4acfaef8c2526d3fbbc50e99c435
mavis.tan <mavis@crypto.com>
2020-11-27 09:44:20 +0800

    no need overwrite base interface when no change

--- types/config.ts
+++ types/config.ts
@@ -2,12 +2,6 @@ import { Monitor } from 'redis-smq-monitor';
 
 export interface ConfigInterface extends Monitor.ConfigInterface {
     namespace?: string,
-    redis: {
-        driver: Monitor.RedisDriver,
-        options: {
-            [key:string]: any
-        }
-    },
     log?: {
         enabled: boolean,
         options: {
