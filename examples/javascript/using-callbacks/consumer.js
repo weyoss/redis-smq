257cc2ed67217cb762c0aaea54e75267b8172b59
Weyoss <weyoss@outlook.com>
2022-05-13 10:17:52 +0100

    Update examples

--- examples/javascript/using-callbacks/consumer.js
+++ examples/javascript/using-callbacks/consumer.js
@@ -3,48 +3,47 @@ const { Consumer } = require('../../..'); // require('redis-smq')
 const consumer = new Consumer();
 
 exports.consume = function consume(cb) {
-  consumer.consume(
-    // Using the default namespace from the config
-    // Same as { name: 'test_queue', ns: 'ns1' })
-    'test_queue',
-    (message, cb) => {
-      /* eslint class-methods-use-this: 0 */
-      //  console.log(`Got message to consume: `, JSON.stringify(message));
-      //  throw new Error('TEST!');
-      //  cb(new Error('TEST!'));
-      //  const timeout = parseInt(Math.random() * 100);
-      //  setTimeout(() => {
-      //      cb();
-      //  }, timeout);
-      cb();
-    },
-    (err, isRunning) => {
-      if (err) console.log(err);
-      else {
-        // As the consumer is not running, the message handler does not run immediately
-        // At this time the message handler is just registered
-        console.log(`Is running? ${isRunning}`); // false
-      }
-    },
-  );
-
+  // starting the consumer and then registering a message handler
   consumer.run((err) => {
     if (err) cb(err);
-    else {
+    else
       consumer.consume(
-        'another_queue',
+        // Using the default namespace from the config
+        // Same as { name: 'test_queue', ns: 'ns1' })
+        'test_queue',
         (message, cb) => {
+          /* eslint class-methods-use-this: 0 */
+          //  console.log(`Got message to consume: `, JSON.stringify(message));
+          //  throw new Error('TEST!');
+          //  cb(new Error('TEST!'));
+          //  const timeout = parseInt(Math.random() * 100);
+          //  setTimeout(() => {
+          //      cb();
+          //  }, timeout);
           cb();
         },
         (err, isRunning) => {
-          if (err) cb(err);
+          if (err) console.log(err);
           else {
-            // At this time the message handler is expected to be running
             console.log(`Is running? ${isRunning}`); // true
             cb();
           }
         },
       );
-    }
   });
+
+  // OR
+  //
+  // registering the message handler and then starting the consumer
+  // consumer.consume(
+  //   'test_queue',
+  //   (message, cb) => cb(),
+  //   (err, isRunning) => {
+  //     if (err) console.log(err);
+  //     else {
+  //       console.log(`Is running? ${isRunning}`); // false
+  //       consumer.run(cb);
+  //     }
+  //   },
+  // );
 };
