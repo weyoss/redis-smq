6599a430cfa74019c43854b496298c501b9806f2
Weyoss <weyoss@outlook.com>
2022-06-19 11:48:10 +0100

    Clean up examples

--- examples/typescript/using-callbacks/index.ts
+++ examples/typescript/using-callbacks/index.ts
@@ -2,5 +2,13 @@ import { init } from './setup';
 import { produce } from './producer';
 import { consume } from './consumer';
 
-// To simplify the example, we are ignoring errors from callbacks
-init(() => produce(() => consume(() => void 0)));
+// For simplicity, we are using nested callbacks
+init((err) => {
+  if (err) throw err;
+  produce((err) => {
+    if (err) throw err;
+    consume((err) => {
+      if (err) throw err;
+    });
+  });
+});
