23eca90221d24d1dbdaa1ce532d3321db024297f
Weyoss <weyoss@outlook.com>
2022-08-31 21:57:11 +0100

    test(exchange): increase code coverage

---
+++ tests/tests/exchanges/direct-exchange/test00001.test.ts
@@ -0,0 +1,17 @@
+import { RedisKeysError } from '../../../../src/common/redis-keys/redis-keys.error';
+import { DirectExchange } from '../../../../src/lib/exchange/direct.exchange';
+import { promisifyAll } from 'bluebird';
+import { getRedisInstance } from '../../../common/redis';
+import { requiredConfig } from '../../../common/config';
+
+test('DirectExchange', async () => {
+  expect(() => new DirectExchange('!@223333')).toThrow(RedisKeysError);
+  expect(() => new DirectExchange('223333.')).toThrow(RedisKeysError);
+  expect(() => new DirectExchange('223333.w')).not.toThrow();
+  expect(() => new DirectExchange('223333.w_e')).not.toThrow();
+  expect(() => new DirectExchange('223333.w-e')).not.toThrow();
+  const e = promisifyAll(new DirectExchange('queue_a'));
+  const client = await getRedisInstance();
+  const r = await e.getQueuesAsync(client, requiredConfig);
+  expect(r).toEqual([{ name: 'queue_a', ns: 'testing' }]);
+});
---
+++ tests/tests/exchanges/topic-exchange/test00006.test.ts
@@ -0,0 +1,15 @@
+import { TopicExchange } from '../../../../src/lib/exchange/topic.exchange';
+import { EExchangeType, ITopicExchangeParams } from '../../../../types';
+import { InvalidExchangeDataError } from '../../../../src/lib/exchange/errors/invalid-exchange-data.error';
+
+test('TopicExchange: fromJSON()', async () => {
+  const json: ITopicExchangeParams = {
+    bindingParams: 'w123.1',
+    type: EExchangeType.TOPIC,
+    destinationQueue: null,
+    exchangeTag: '123',
+  };
+  expect(() => TopicExchange.fromJSON({})).toThrow(InvalidExchangeDataError);
+  const e = TopicExchange.fromJSON(json);
+  expect(e.toJSON()).toEqual(json);
+});
