0ec722edc908fa3149c12750317eb567bc9a1586
Weyoss <weyoss@outlook.com>
2022-09-01 14:44:04 +0100

    test(exchange): fix test errors

--- tests/tests/exchanges/topic-exchange/test00001.test.ts
+++ tests/tests/exchanges/topic-exchange/test00001.test.ts
@@ -4,7 +4,8 @@ import { RedisKeysError } from '../../../../src/common/redis-keys/redis-keys.err
 test('TopicExchange: topic validation', async () => {
   expect(() => new TopicExchange('!@223333')).toThrow(RedisKeysError);
   expect(() => new TopicExchange('223333.')).toThrow(RedisKeysError);
-  expect(() => new TopicExchange('223333.w')).not.toThrow();
-  expect(() => new TopicExchange('223333.w_e')).not.toThrow();
-  expect(() => new TopicExchange('223333.w-e')).not.toThrow();
+  expect(() => new TopicExchange('223333.w')).toThrow(RedisKeysError);
+  expect(() => new TopicExchange('a223333.w')).not.toThrow();
+  expect(() => new TopicExchange('a223333.w_e')).not.toThrow();
+  expect(() => new TopicExchange('a223333.w-e')).not.toThrow();
 });
