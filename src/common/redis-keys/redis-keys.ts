3389216dd5e6b95b9457b7fdfa82fdb0773dacaa
Weyoss <weyoss@protonmail.com>
2022-09-16 18:15:42 +0100

    refactor: bump up redis keys version

--- src/common/redis-keys/redis-keys.ts
+++ src/common/redis-keys/redis-keys.ts
@@ -5,7 +5,7 @@ import { TQueueParams } from '../../../types';
 const keySegmentSeparator = ':';
 
 // Key prefix
-const nsPrefix = 'redis-smq-v7';
+const nsPrefix = 'redis-smq-v7.1';
 
 // Namespaces
 const globalNamespace = 'global';
