509909ac2ff93b18fd7740228efa292883c07b04
Weyoss <weyoss@outlook.com>
2022-09-21 13:46:52 +0100

    test(FanOutExchangeManager): increase code coverage

--- tests/tests/exchanges/fanout-exchange/test00007.test.ts
+++ tests/tests/exchanges/fanout-exchange/test00007.test.ts
@@ -19,6 +19,7 @@ test('FanOutExchange: creating and deleting an exchange', async () => {
   const { queue } = await getQueueManager();
   await queue.createAsync(q1, false);
   await fanOutExchangeManager.bindQueueAsync(q1, e1);
+  await fanOutExchangeManager.bindQueueAsync(q1, e1);
 
   const r4 = await fanOutExchangeManager.getExchangeQueuesAsync(e1);
   expect(r4).toEqual([q1]);
@@ -34,4 +35,13 @@ test('FanOutExchange: creating and deleting an exchange', async () => {
   await expect(fanOutExchangeManager.deleteExchangeAsync(e2)).rejects.toThrow(
     `Exchange has 1 bound queue(s). Unbind all queues before deleting the exchange.`,
   );
+
+  await fanOutExchangeManager.unbindQueueAsync(q1, e2);
+  await expect(fanOutExchangeManager.unbindQueueAsync(q1, e2)).rejects.toThrow(
+    `Queue ${q1.name}@${q1.ns} is not bound to [e2] exchange.`,
+  );
+
+  await fanOutExchangeManager.deleteExchangeAsync(e2);
+  const r7 = await fanOutExchangeManager.getExchangesAsync();
+  expect(r7).toEqual(['e1']);
 });
