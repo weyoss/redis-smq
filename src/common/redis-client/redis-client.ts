9161d5c4905a096199317ebaa173e5393cae3a76
Weyoss <weyoss@outlook.com>
2023-02-15 22:39:28 +0300

    fix: use path.resolve() to fix 'no such file or directory' error
    See https://github.com/weyoss/redis-smq/issues/99

--- src/common/redis-client/redis-client.ts
+++ src/common/redis-client/redis-client.ts
@@ -1,5 +1,6 @@
 import { RedisClient } from 'redis-smq-common';
 import * as fs from 'fs';
+import { resolve } from 'path';
 
 export enum ELuaScriptName {
   ENQUEUE_SCHEDULED_MESSAGE = 'ENQUEUE_SCHEDULED_MESSAGE',
@@ -13,29 +14,35 @@ export enum ELuaScriptName {
 
 RedisClient.addScript(
   ELuaScriptName.ENQUEUE_SCHEDULED_MESSAGE,
-  fs.readFileSync(`${__dirname}/lua/enqueue-scheduled-message.lua`).toString(),
+  fs
+    .readFileSync(resolve(__dirname, './lua/enqueue-scheduled-message.lua'))
+    .toString(),
 );
 RedisClient.addScript(
   ELuaScriptName.PUBLISH_MESSAGE,
-  fs.readFileSync(`${__dirname}/lua/publish-message.lua`).toString(),
+  fs.readFileSync(resolve(__dirname, './lua/publish-message.lua')).toString(),
 );
 RedisClient.addScript(
   ELuaScriptName.REQUEUE_MESSAGE,
-  fs.readFileSync(`${__dirname}/lua/requeue-message.lua`).toString(),
+  fs.readFileSync(resolve(__dirname, './lua/requeue-message.lua')).toString(),
 );
 RedisClient.addScript(
   ELuaScriptName.SCHEDULE_MESSAGE,
-  fs.readFileSync(`${__dirname}/lua/schedule-message.lua`).toString(),
+  fs.readFileSync(resolve(__dirname, './lua/schedule-message.lua')).toString(),
 );
 RedisClient.addScript(
   ELuaScriptName.HAS_QUEUE_RATE_EXCEEDED,
-  fs.readFileSync(`${__dirname}/lua/has-queue-rate-exceeded.lua`).toString(),
+  fs
+    .readFileSync(resolve(__dirname, './lua/has-queue-rate-exceeded.lua'))
+    .toString(),
 );
 RedisClient.addScript(
   ELuaScriptName.CREATE_QUEUE,
-  fs.readFileSync(`${__dirname}/lua/create-queue.lua`).toString(),
+  fs.readFileSync(resolve(__dirname, './lua/create-queue.lua')).toString(),
 );
 RedisClient.addScript(
   ELuaScriptName.INIT_CONSUMER_QUEUE,
-  fs.readFileSync(`${__dirname}/lua/init-consumer-queue.lua`).toString(),
+  fs
+    .readFileSync(resolve(__dirname, './lua/init-consumer-queue.lua'))
+    .toString(),
 );
