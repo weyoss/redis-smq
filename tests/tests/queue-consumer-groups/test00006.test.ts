0da9bfb3fec9a5edbe3f4d4dc60ac4d666096577
Weyoss <weyoss@outlook.com>
2024-03-30 07:30:46 +0300

    test: increase code coverage

---
+++ tests/tests/queue-consumer-groups/test00006.test.ts
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c)
+ * Weyoss <weyoss@protonmail.com>
+ * https://github.com/weyoss
+ *
+ * This source code is licensed under the MIT license found in the LICENSE file
+ * in the root directory of this source tree.
+ */
+
+import { expect, test } from '@jest/globals';
+import bluebird from 'bluebird';
+import {
+  Consumer,
+  ConsumerGroupIdNotSupportedError,
+  EQueueDeliveryModel,
+  EQueueType,
+  IQueueParams,
+} from '../../../src/lib/index.js';
+import { getQueue } from '../../common/queue.js';
+
+test('ConsumerGroupIdNotSupportedError', async () => {
+  const queue1: IQueueParams = {
+    name: 'test-queue',
+    ns: 'ns1',
+  };
+
+  const queue = await getQueue();
+  await queue.saveAsync(
+    queue1,
+    EQueueType.PRIORITY_QUEUE,
+    EQueueDeliveryModel.POINT_TO_POINT,
+  );
+
+  const consumer1 = bluebird.promisifyAll(new Consumer());
+  await consumer1.consumeAsync(
+    { queue: queue1, groupId: 'my-group-1' },
+    (msg, cb) => cb(),
+  );
+
+  await expect(consumer1.runAsync()).rejects.toThrow(
+    ConsumerGroupIdNotSupportedError,
+  );
+  await consumer1.shutdownAsync();
+});
