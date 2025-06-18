c44887a437e131334bb096547d2eb8df7e8f50fd
Weyoss <weyoss@protonmail.com>
2025-06-18 16:30:54 +0300

    docs(redis-smq-common): add missing copyright headers

--- packages/redis-smq-common/src/redis-server/redis-server.ts
+++ packages/redis-smq-common/src/redis-server/redis-server.ts
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c)
+ * Weyoss <weyoss@protonmail.com>
+ * https://github.com/weyoss
+ *
+ * This source code is licensed under the MIT license found in the LICENSE file
+ * in the root directory of this source tree.
+ */
+
 import { ChildProcess, spawn } from 'child_process';
 import { exec } from 'node:child_process';
 import { promisify } from 'node:util';
