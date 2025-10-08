7c774ab3b669b082264166a436f25ebd37fe3a7a
Weyoss <weyoss@protonmail.com>
2025-10-08 20:01:17 +0200

    refactor(redis-smq): update copyright headers

--- packages/redis-smq/src/exchange/_/_validate-queue-binding.ts
+++ packages/redis-smq/src/exchange/_/_validate-queue-binding.ts
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c)
+ * Weyoss <weyoss@protonmail.com>
+ * https://github.com/weyoss
+ *
+ * This source code is licensed under the MIT license found in the LICENSE file
+ * in the root directory of this source tree.
+ */
+
 import { async, ICallback, IRedisClient } from 'redis-smq-common';
 import { ExchangeError, ExchangeNotFoundError } from '../../errors/index.js';
 import { _getQueueProperties } from '../../queue-manager/_/_get-queue-properties.js';
--- packages/redis-smq/src/exchange/exchange-direct/_/_get-direct-exchange-routing-keys.ts
+++ packages/redis-smq/src/exchange/exchange-direct/_/_get-direct-exchange-routing-keys.ts
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c)
+ * Weyoss <weyoss@protonmail.com>
+ * https://github.com/weyoss
+ *
+ * This source code is licensed under the MIT license found in the LICENSE file
+ * in the root directory of this source tree.
+ */
+
 import { ICallback, IRedisClient } from 'redis-smq-common';
 import { redisKeys } from '../../../common/redis-keys/redis-keys.js';
 import { _parseExchangeParams } from '../../_/_parse-exchange-params.js';
