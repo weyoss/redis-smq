1407e75f5139b59ae5c292a94aa5d79118a0de84
Weyoss <weyoss@outlook.com>
2025-10-27 00:32:02 +0100

    test(redis-smq-web-server): add comprehensive E2E test suite

---
+++ packages/redis-smq-web-server/tests/setup.ts
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c)
+ * Weyoss <weyoss@protonmail.com>
+ * https://github.com/weyoss
+ *
+ * This source code is licensed under the MIT license found in the LICENSE file
+ * in the root directory of this source tree.
+ */
+
+import { afterAll, afterEach, beforeAll, beforeEach } from 'vitest';
+
+import {
+  initializeRedis,
+  shutDownRedisServer,
+} from './common/start-redis-server.js';
+import {
+  getRedisClientInstance,
+  shutdownRedisClient,
+} from './common/redis-client.js';
+
+beforeAll(async () => {
+  await initializeRedis();
+});
+
+afterAll(async () => {
+  await shutdownRedisClient();
+  await shutDownRedisServer();
+});
+
+beforeEach(async () => {
+  const redis = await getRedisClientInstance();
+  await redis.flushallAsync();
+});
+
+afterEach(async () => void 0);
