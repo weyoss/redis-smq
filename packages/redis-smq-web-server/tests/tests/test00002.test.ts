8cf333150129aabd91b68204cf0eca428888efc7
Weyoss <weyoss@outlook.com>
2025-10-27 12:41:11 +0100

    chore: add copyright headers to source files

--- packages/redis-smq-web-server/tests/tests/test00002.test.ts
+++ packages/redis-smq-web-server/tests/tests/test00002.test.ts
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c)
+ * Weyoss <weyoss@protonmail.com>
+ * https://github.com/weyoss
+ *
+ * This source code is licensed under the MIT license found in the LICENSE file
+ * in the root directory of this source tree.
+ */
+
 import { describe, it, expect, beforeEach, afterEach } from 'vitest';
 import request from 'supertest';
 import { startApiServer, stopApiServer } from '../common/start-api-server.js';
