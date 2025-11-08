d19ea47c3c7790f34e801a57e7e7fdc858c5270d
Weyoss <weyoss@protonmail.com>
2025-11-08 10:10:35 +0100

    chore: update copyright header

--- scripts/update-readme-files.sh
+++ scripts/update-readme-files.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 # shellcheck shell=bash
+
+#
+# Copyright (c)
+# Weyoss <weyoss@protonmail.com>
+# https://github.com/weyoss
+#
+# This source code is licensed under the MIT license found in the LICENSE file
+# in the root directory of this source tree.
+#
+
 set -euo pipefail
 
 repo_root="$(git rev-parse --show-toplevel)"
