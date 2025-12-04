cf75fa2d4c08f07333863813a56404db0b524d54
Weyoss <weyoss@outlook.com>
2025-12-04 12:22:01 +0100

    fix(redis-smq): fail when acked message audit is not enabled

--- packages/redis-smq/src/queue-acknowledged-messages/queue-acknowledged-messages.ts
+++ packages/redis-smq/src/queue-acknowledged-messages/queue-acknowledged-messages.ts
@@ -10,13 +10,19 @@
 import { QueueMessagesAbstract } from '../common/queue-messages/queue-messages-abstract.js';
 import { QueueStorageList } from '../common/queue-messages/queue-storage/queue-storage-list.js';
 import { MessageManager } from '../message-manager/index.js';
+import { TQueueExtendedParams } from '../queue-manager/index.js';
+import { ICallback } from 'redis-smq-common';
+import { IPaginationPage } from '../common/index.js';
+import { IMessageTransferable } from '../message/index.js';
+import { Configuration } from '../config/index.js';
+import { AcknowledgedMessageAuditNotEnabledError } from '../errors/index.js';
 
 /**
  * Manages acknowledged messages in a queue.
  *
  * Acknowledged messages are those that have been successfully processed by consumers
  * and can be safely removed from the active queue. This class allows for tracking
- * and management of these messages when the system is configured to store them.
+ * and management of these messages when the system is configured to audit them.
  *
  * @extends QueueMessagesAbstract
  * @see /packages/redis-smq/docs/configuration.md#message-audit
@@ -25,5 +31,31 @@ export class QueueAcknowledgedMessages extends QueueMessagesAbstract {
   constructor() {
     super(new QueueStorageList(), new MessageManager(), 'keyQueueAcknowledged');
     this.logger.debug('QueueAcknowledgedMessages initialized');
+  }
+
+  override getMessages(
+    queue: TQueueExtendedParams,
+    page: number,
+    pageSize: number,
+    cb: ICallback<IPaginationPage<IMessageTransferable>>,
+  ) {
+    const cfg = Configuration.getConfig();
+    if (!cfg.messageAudit.acknowledgedMessages.enabled)
+      return cb(new AcknowledgedMessageAuditNotEnabledError());
+    super.getMessages(queue, page, pageSize, cb);
+  }
+
+  override purge(queue: TQueueExtendedParams, cb: ICallback) {
+    const cfg = Configuration.getConfig();
+    if (!cfg.messageAudit.acknowledgedMessages.enabled)
+      return cb(new AcknowledgedMessageAuditNotEnabledError());
+    super.purge(queue, cb);
+  }
+
+  override countMessages(queue: TQueueExtendedParams, cb: ICallback<number>) {
+    const cfg = Configuration.getConfig();
+    if (!cfg.messageAudit.acknowledgedMessages.enabled)
+      return cb(new AcknowledgedMessageAuditNotEnabledError());
+    super.countMessages(queue, cb);
   }
 }
