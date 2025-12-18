53a53d7149b6ece2799155ba75bb3d1058b79f99
Weyoss <weyoss@outlook.com>
2025-12-18 11:30:57 +0100

    test(redis-smq-common): test async.series() error handling

---
+++ packages/redis-smq-common/tests/async/test00017.test.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c)
+ * Weyoss <weyoss@protonmail.com>
+ * https://github.com/weyoss
+ *
+ * This source code is licensed under the MIT license found in the LICENSE file
+ * in the root directory of this source tree.
+ */
+
+import { expect, it } from 'vitest';
+import { async } from '../../src/async/async.js';
+import { ICallback } from '../../src/async/index.js';
+
+it('async.series: error handling', async () => {
+  let count = 0;
+  const p = new Promise<void>((resolve, reject) => {
+    async.series(
+      [
+        (cb: ICallback<void>) => {
+          expect(count).toBe(0);
+          count += 1;
+          cb();
+        },
+        (cb: ICallback<void>) => {
+          cb(new Error('explicit error'));
+        },
+        (cb: ICallback<void>) => {
+          expect(count).toBe(2);
+          count += 1;
+          cb();
+        },
+      ],
+      (err) => {
+        if (err) reject(err);
+        else resolve();
+      },
+    );
+  });
+  await expect(p).rejects.toThrow('explicit error');
+  expect(count).toBe(1);
+});
