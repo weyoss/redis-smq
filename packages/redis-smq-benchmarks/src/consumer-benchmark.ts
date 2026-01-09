bc84c452a4e0e8b8be6a462463ddc32837112d65
Weyoss <weyoss@outlook.com>
2026-01-09 11:54:57 +0100

    chore(redis-smq-benchmarks): update copyright headers

--- packages/redis-smq-benchmarks/src/consumer-benchmark.ts
+++ packages/redis-smq-benchmarks/src/consumer-benchmark.ts
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c)
+ * Weyoss <weyoss@outlook.com>
+ * https://github.com/weyoss
+ *
+ * This source code is licensed under the MIT license found in the LICENSE file
+ * in the root directory of this source tree.
+ */
+
 import { async, ICallback } from 'redis-smq-common';
 import { ensureQueue } from './helpers/ensure-queue.js';
 import { prefillQueue } from './helpers/prefill-queue.js';
